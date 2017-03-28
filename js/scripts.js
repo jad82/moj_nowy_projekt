13978e92e45042996547b34294ed7ca31a21be27
Jan Drzymała <jan.drzymala@yahoo.com>
2017-03-28 12:42:53 +0200

    new file with instruction if

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-// plik scripts.js - pole trójkąta
-var a = prompt('Podaj wartość dla podstawy trójkąta');
-	h = prompt('Podaj wartość wysokości trójkąta')
-triangleArea = a*h/2
-alert('Pole trójkąta wynosi:' + triangleArea);
-//Komunikat dla konsoli
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+// plik scripts.js - sprawdzanie, czy wynik jest dodatni, czy ujemny
+var a = prompt('Podaj wartość a');
+	b = prompt('Podaj wartość b')
+value = (a * a) + (2 * a * b) - (b * b)
+	console.log('Sprawdzenie wartości')
+var positiveNegative = (value<0) ? alert('wartość ujemna') : alert('Wartość dodatnia')
+	console.log('Sprawdzenie warunku i podanie informacji o wyniku, który wynosi'+ value);
