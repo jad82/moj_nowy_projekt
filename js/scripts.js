458590c4f38678086ad6c4c36f82251e7ebfe744
Jan Drzymała <jan.drzymala@yahoo.com>
2017-04-04 14:42:00 +0200

    var-if script

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // plik scripts.js - sprawdzanie, czy wynik jest dodatni, czy ujemny
 var a = prompt('Podaj wartość a');
-	b = prompt('Podaj wartość b')
+var b = prompt('Podaj wartość b');
 value = (a * a) + (2 * a * b) - (b * b)
-	console.log('Sprawdzenie wartości')
-var positiveNegative = (value<0) ? alert('wartość ujemna') : alert('Wartość dodatnia')
-	console.log('Sprawdzenie warunku i podanie informacji o wyniku, który wynosi'+ value);
+console.log('Sprawdzenie wartości')
+var positiveNegative = (value < 0) ? alert('wartość ujemna') : alert('Wartość dodatnia')
+console.log('Sprawdzenie warunku i podanie informacji o wyniku, który wynosi'+ value); 
