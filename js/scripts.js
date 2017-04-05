a41dbe4ef2695532ab0f9d82dc2c28aeb92e5054
Jan Drzymała <jan.drzymala@yahoo.com>
2017-04-05 14:07:43 +0200

    deklaracje i formatowanie poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // plik scripts.js - pole trójkąta
 var a = prompt('Podaj wartość dla podstawy trójkąta');
-	h = prompt('Podaj wartość wysokości trójkąta')
-triangleArea = a*h/2
+	h = prompt('Podaj wartość wysokości trójkąta');
+triangleArea = a * h / 2
 alert('Pole trójkąta wynosi:' + triangleArea);
 //Komunikat dla konsoli
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
