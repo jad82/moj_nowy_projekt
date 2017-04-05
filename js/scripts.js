e9fd43a7475f21d9a7f31078db08e71ace1abc7b
Jan Drzymała <jan.drzymala@yahoo.com>
2017-04-05 14:47:46 +0200

    dodany case 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 // plik scripts.js - sprawdzanie, czy wynik jest dodatni, czy ujemny
 var a = prompt('Podaj wartość a');
-var b = prompt('Podaj wartość b');
-value = (a * a) + (2 * a * b) - (b * b)
-console.log('Sprawdzenie wartości')
-var positiveNegative = (value < 0) ? alert('wartość ujemna') : alert('Wartość dodatnia')
-console.log('Sprawdzenie warunku i podanie informacji o wyniku, który wynosi'+ value); 
+var	b = prompt('Podaj wartość b');
+var value = (a * a) + (2 * a * b) - (b * b);
+if (value < 0) {
+	alert('wynik ujemny')
+}
+else if (value > 0 ) {
+	alert('wynik dodatni')
+}
+else {
+alert('wynik równy zero')
+}
