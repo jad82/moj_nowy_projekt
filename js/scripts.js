8ebb415ccac983625f54a338e2ae262300e1e69b
JanekDrzymala <jan.drzymala@live.com>
2017-04-05 02:33:55 +0200

    strings - ex8.8

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach'; 
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+var replacedAnimal = text.replace('Papugi', animalUpperCased);
+console.log(text.length);
+console.log(animalUpperCased.length);
+console.log(replacedAnimal.slice(0,45));
