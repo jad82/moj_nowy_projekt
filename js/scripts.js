951043d3dc50b8b3035fa14b3323ad6f71256e2d
Jan Drzymała <jan.drzymala@yahoo.com>
2017-04-11 13:03:33 +0200

    podmieniona zmienna

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var x = prompt('Jak wysoką choinkę chcesz uzyskać?');
 function drawTree(x) {
 	for ( var i = 0 ; i = x ; i++) { 
 	var star = ' ';
-	for ( var j = 0; j = x; star += ' *') 
+	for ( var j = 0; j = i; star += ' *') 
 console.log (star);
 }
 };
