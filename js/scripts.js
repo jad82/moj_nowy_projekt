2201fd0741216bd7029e81bdb7af130a57475a7a
Jan Drzymała <jan.drzymala@yahoo.com>
2017-04-11 12:50:11 +0200

    pętla z drzewkiem

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var x = prompt('Jak wysoką choinkę chcesz uzyskać?');
+function drawTree(x) {
+	for ( var i = 0 ; i = x ; i++) { 
+	var star = ' ';
+	for ( var j = 0; j = x; star += ' *') 
+console.log (star);
+}
+};
+console.log (drawTree(x));
